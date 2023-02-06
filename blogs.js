e7d9fd3df1bbfd8e3982a097576e2a2bc33ee57f
enockkipkoech19@gmail.com <enockkipkoech19@gmail.com>
2023-02-06 11:46:46 +0300

    routing

--- blogs.js
+++ blogs.js
@@ -1,6 +1,9 @@
 import fetch from "node-fetch";
+import express from "express";
+const app = express();
 
-const query = `
+export const getBlogs = async () => {
+	const query = `
 	query GetUserArticles {
 		user(username: "nodeG") {
 			publication {
@@ -14,8 +17,6 @@ const query = `
 		}
 	}
 `;
-
-export const getBlogs = async (query) => {
 	try {
 		const data = await fetch("https://api.hashnode.com", {
 			method: "POST",
@@ -33,11 +34,26 @@ export const getBlogs = async (query) => {
 			.then((result) => {
 				return result.data.user.publication;
 			});
-		console.log(data);
+		// console.log(data);
 		return data;
 	} catch (error) {
 		console.log(`Error Querying BlogPosts!`, error);
 	}
 };
 
-getBlogs(query);
+app.get("/", (req, res) => {
+	const posts = getBlogs();
+	console.log(posts);
+	res.send({
+		status: 200,
+		data: posts,
+	});
+});
+
+// PORT Connection
+const PORT = 5000;
+app.listen(PORT, () => {
+	console.log(
+		`\n🤝 🤝 [SERVER:]  Listening on port: http://127.0.0.1:${PORT}/ 🤝 🤝 `,
+	);
+});
